4b2ceb2057b589369b3c1b2509112ea603dc91ef
Naomi Lambert <naomi.lambert.codes@gmail.com>
2020-05-16 16:38:48 +0100

    FIXED!

--- src/com/nl/csv2xml/csv2xml.java
+++ src/com/nl/csv2xml/csv2xml.java
@@ -47,25 +47,23 @@ public class csv2xml{
 
 	/** Line length constansts **/
 
-	private static final int QUESTION_WITH_COMMENT = 11;
+	private static final int STANDARD = 11;
 	private static final int QUESTION_WITH_OPTIONAL = 13;
-	private static final int RESPONSE_WITH_COMMENT = 12;
 
 	/** csv file constant **/
 	private static final int QUESTION = 0;
 	private static final int QUESTION_COMMENT = 1;
 	private static final int YES = 2;
-	private static final int YES_COMMENT = 5;
+	private static final int YES_COMMENT = 3;
 	private static final int NO = 4;
-	private static final int NO_COMMENT = 5;
+	private static final int NO_COMMENT = 6;
 	private static final int SIT = 6;
-	private static final int SIT_COMMENT = 5;
+	private static final int SIT_COMMENT = 7;
 	private static final int STAND = 8;
-	private static final int STAND_COMMENT = 5;
+	private static final int STAND_COMMENT = 9;
 	private static final int BLANK = 10;
-	private static final int BLANK_COMMENT = 5;
+	private static final int BLANK_COMMENT = 11;
 	private static final int OPTIONAL = 12;
-	private static final int OPTIONAL_COMMENT = 5;
 
 
 	/*----------------------------- General Methods ----------------------------*/
@@ -133,26 +131,23 @@ public class csv2xml{
 
 		while (scannerCommentsFile.hasNextLine()){
 			String commentsData = scannerCommentsFile.nextLine();
-			String delims = ",";
+			String delims = ",(?=([^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)";
 			String[] commentTokens = commentsData.split(delims);
 
-			if (commentLine!=0){
-				Element comment = document.createElement("aComment");
+			for (String c : commentTokens)
+				System.out.println(c.replace("\"", ""));
 
-				Element commentID = document.createElement("commentNoteID");
+			if (commentLine!=0){
+				Element comment = document.createElement("comment");
+
+				Element commentID = document.createElement("commentID");
 				commentID.appendChild(document.createTextNode(commentTokens[0]));
 				comment.appendChild(commentID);
 
-				String commentText = null;
-				Element commentDetails = document.createElement("commentDetails");
+				Element commentDetails = document.createElement("comment");
 
-				if (commentTokens.length == 2) {
-					commentText = (commentTokens[1]).replace("\"", "");
-				} else if (commentTokens.length == 3) {
-					commentText = (commentTokens[1] + commentTokens[2]).replace("\"", "");
-				}
-				
-				commentDetails.appendChild(document.createTextNode(commentText));
+
+				commentDetails.appendChild(document.createTextNode((commentTokens[1]).replace("\"", "")));
 				comment.appendChild(commentDetails);
 
 				comments.appendChild(comment);
@@ -181,13 +176,17 @@ public class csv2xml{
 		while (scannerDataFile.hasNextLine()){
 
 			String information = scannerDataFile.nextLine();	// extract the line of data
-			String delims = ",";								// csv files are delimeted by commas
-			String[] rowTokens = information.split(delims);
-			System.out.println("Line " + line + " : " + rowTokens.length);
+			String delims = ",(?=([^\\\"]*\\\"[^\\\"]*\\\")*[^\\\"]*$)";								// csv files are delimeted by commas
+			String[] questionTokens = information.split(delims);
+			System.out.println("Line " + line + " : " + questionTokens.length);
+
+
 
 			if (line > 0){	// counting from 1 discards the header row
 
-				//appendQuestionToQuestions(questions, rowTokens);
+				for (String q : questionTokens)
+					System.out.print(q + ", ");
+				appendQuestionToQuestions(questions, questionTokens);
 
 			}
 			line++; // increment line count before processing next line    
@@ -197,66 +196,102 @@ public class csv2xml{
 	}
 
 
-	private static void appendQuestionToQuestions(Element questions, String[] rowTokens) {
+	private static void appendQuestionToQuestions(Element questions, String[] questionTokens) {
 		// create question element
 		Element question = document.createElement("question");
 
 		// create phrase and append
 		Element phrase = document.createElement("phrase");
-		phrase.appendChild(document.createTextNode(rowTokens[QUESTION]));
+		phrase.appendChild(document.createTextNode(questionTokens[QUESTION].replace("\"", "")));
 		question.appendChild(phrase);
 
-		if (!(rowTokens[QUESTION_COMMENT].equals(""))){
+		if (!(questionTokens[QUESTION_COMMENT].equals(""))){
 			Element commentID = document.createElement("commentID");
-			commentID.appendChild(document.createTextNode(rowTokens[QUESTION_COMMENT]));
+			commentID.appendChild(document.createTextNode(questionTokens[QUESTION_COMMENT]));
 			question.appendChild(commentID);}
 
 		// create answers child of question and append to question
 		Element answers = document.createElement("answers");
 
+		appendAnswers(answers, questionTokens);
 
-		if (rowTokens.length == 11){
-			Element yesno = document.createElement("yesno");
-			Element yes = document.createElement("yes");
-			yes.appendChild(document.createTextNode(rowTokens[2]));
-			yesno.appendChild(yes);
-
-			Element no = document.createElement("no");
-			no.appendChild(document.createTextNode(rowTokens[4]));
-			yesno.appendChild(no);
-			responses.appendChild(yesno);
-
-			if (line == 8){
-				Element commentID1 = document.createElement("commentIDno");
-				commentID1.appendChild(document.createTextNode(rowTokens[5]));
-				yesno.appendChild(commentID1);
-			}
-		} else {
-			Element sitstand = document.createElement("sitstand");
-			Element sit = document.createElement("sit");
-			sit.appendChild(document.createTextNode(rowTokens[6]));
-			sitstand.appendChild(sit);
-
-			Element stand = document.createElement("stand");
-			stand.appendChild(document.createTextNode(rowTokens[8]));
-			sitstand.appendChild(stand);
-			responses.appendChild(sitstand);
-		}	
-
-		Element blank = document.createElement("blank");
-		blank.appendChild(document.createTextNode(rowTokens[10]));
-		responses.appendChild(blank);
-
-		if (rowTokens.length == 13){
-			Element optional = document.createElement("optional");
-			optional.appendChild(document.createTextNode(rowTokens[12]));
-			responses.appendChild(optional);
-		}
-
-		question.appendChild(responses);
+		question.appendChild(answers);
 
 		questions.appendChild(question);
 
+	}
+
+
+	private static void appendAnswers(Element answers, String[] questionTokens) {
+
+		if (!(questionTokens[YES].equals(""))) {
+			Element yes = document.createElement("yes");
+			yes.appendChild(document.createTextNode(questionTokens[YES]));
+			answers.appendChild(yes);
+		}
+
+		if (!(questionTokens[YES_COMMENT].equals(""))) {
+			Element yescomment = document.createElement("yescomment");
+			yescomment.appendChild(document.createTextNode(questionTokens[YES_COMMENT]));
+			answers.appendChild(yescomment);
+		}
+
+		if (!(questionTokens[NO].equals(""))) {
+			Element no = document.createElement("no");
+			no.appendChild(document.createTextNode(questionTokens[NO]));
+			answers.appendChild(no);
+		}
+
+		if (!(questionTokens[NO_COMMENT].equals(""))) {
+			Element nocomment = document.createElement("nocomment");
+			nocomment.appendChild(document.createTextNode(questionTokens[NO_COMMENT]));
+			answers.appendChild(nocomment);
+		}
+
+		if (!(questionTokens[SIT].equals(""))) {
+			Element sit = document.createElement("sit");
+			sit.appendChild(document.createTextNode(questionTokens[SIT]));
+			answers.appendChild(sit);
+		}
+
+		if (!(questionTokens[SIT_COMMENT].equals(""))) {
+			Element sitcomment = document.createElement("sitcomment");
+			sitcomment.appendChild(document.createTextNode(questionTokens[SIT_COMMENT]));
+			answers.appendChild(sitcomment);
+		}
+
+		if (!(questionTokens[STAND].equals(""))) {
+			Element stand = document.createElement("stand");
+			stand.appendChild(document.createTextNode(questionTokens[STAND]));
+			answers.appendChild(stand);
+		}
+
+		if (!(questionTokens[STAND_COMMENT].equals(""))) {
+			Element standcomment = document.createElement("standcomment");
+			standcomment.appendChild(document.createTextNode(questionTokens[STAND_COMMENT]));
+			answers.appendChild(standcomment);
+		}
+
+		if (!(questionTokens[BLANK].equals(""))) {
+			Element blank = document.createElement("blank");
+			blank.appendChild(document.createTextNode(questionTokens[BLANK]));
+			answers.appendChild(blank);
+		}
+
+		if ((questionTokens.length > STANDARD) && (questionTokens.length == QUESTION_WITH_OPTIONAL)) {
+			if (!(questionTokens[BLANK_COMMENT].equals(""))) {
+				Element blankcomment = document.createElement("blankcomment");
+				blankcomment.appendChild(document.createTextNode(questionTokens[BLANK_COMMENT]));
+				answers.appendChild(blankcomment);
+			}
+			
+			if (!(questionTokens[OPTIONAL].equals(""))){
+				Element optional = document.createElement("optional");
+				optional.appendChild(document.createTextNode(questionTokens[OPTIONAL]));
+				answers.appendChild(optional);
+			}
+
+		}
 	}
 }
 
