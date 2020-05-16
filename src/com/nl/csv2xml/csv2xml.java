4f344c03d4fdaf7c280eb5836503c8f5ffbb9ef6
Naomi Lambert <naomi.lambert.codes@gmail.com>
2020-05-16 13:19:28 +0100

    refactoring

--- src/com/nl/csv2xml/csv2xml.java
+++ src/com/nl/csv2xml/csv2xml.java
@@ -31,6 +31,10 @@ public class csv2xml{
 
 	/** XML document */
 	private static Document document = null;
+	
+	/** scanners for reading in the files **/
+	private static Scanner scannerDataFile = null;
+	private static Scanner scannerCommentsFile = null;
 
 
 	/** Specified files used for this assignment **/
@@ -40,7 +44,7 @@ public class csv2xml{
 	/** input filename path**/	
 	private static final String DATA_FILENAME = "data/surveydata.csv";
 	private static final String commentsFileName = "data/surveycomments.csv";
-	
+
 	/** csv file constant **/
 	private static final int QUESTION = 0;
 	private static final int QUESTION_COMMENT = 1;
@@ -56,7 +60,7 @@ public class csv2xml{
 	private static final int BLANK_COMMENT = 5;
 	private static final int OPTIONAL = 12;
 	private static final int OPTIONAL_COMMENT = 5;
-	
+
 
 	/*----------------------------- General Methods ----------------------------*/
 
@@ -66,9 +70,7 @@ public class csv2xml{
 	 */
 	public static void main(String[] args)  throws FileNotFoundException {
 
-		/** scanners for reading in the files **/
-		Scanner scannerDataFile = null;
-		Scanner scannerCommentsFile = null;
+		
 
 		/**  **/
 		try {
@@ -91,124 +93,14 @@ public class csv2xml{
 
 			// new document
 			document = builder.newDocument();
-
-			// create library as the root element and add it to the document
+			// create surveydata as the root element and add it to the document
 			Element rootElement = document.createElement("surveydata");
 			document.appendChild(rootElement);    
+			
+			appendSurveyDataToDocument(rootElement);
+			
+			appendCommentsToDocument();
 
-			//create questions child of library
-			Element questions = document.createElement("questions");
-			rootElement.appendChild(questions);
-
-			// counter for iterating lines of the file
-			int line = 0;
-
-			// while there are more lines in the file read
-			// in the date to be transformed to xml
-			while (scannerDataFile.hasNextLine()){
-
-				String information = scannerDataFile.nextLine();
-				String delims = ",";
-				String[] rowTokens = information.split(delims);
-
-
-				
-				if ( line == 0) {
-					
-				} else {	// counting from above 0 discards the header row
-
-					// create question element
-					Element question = document.createElement("question");
-
-					// create phrase, comment and append
-					Element phrase = document.createElement("phrase");
-					phrase.appendChild(document.createTextNode(rowTokens[QUESTION]));
-					question.appendChild(phrase);
-
-					if (!(rowTokens[QUESTION_COMMENT].equals(""))){
-						Element commentID = document.createElement("commentID");
-						commentID.appendChild(document.createTextNode(rowTokens[QUESTION_COMMENT]));
-						question.appendChild(commentID);}
-
-					// create answers child of question and append to question
-					Element answers = document.createElement("answers");
-					
-					i
-					if (rowTokens.length == 11){
-						Element yesno = document.createElement("yesno");
-						Element yes = document.createElement("yes");
-						yes.appendChild(document.createTextNode(rowTokens[2]));
-						yesno.appendChild(yes);
-
-						Element no = document.createElement("no");
-						no.appendChild(document.createTextNode(rowTokens[4]));
-						yesno.appendChild(no);
-						responses.appendChild(yesno);
-
-						if (line == 8){
-							Element commentID1 = document.createElement("commentIDno");
-							commentID1.appendChild(document.createTextNode(rowTokens[5]));
-							yesno.appendChild(commentID1);
-						}
-					} else {
-						Element sitstand = document.createElement("sitstand");
-						Element sit = document.createElement("sit");
-						sit.appendChild(document.createTextNode(rowTokens[6]));
-						sitstand.appendChild(sit);
-
-						Element stand = document.createElement("stand");
-						stand.appendChild(document.createTextNode(rowTokens[8]));
-						sitstand.appendChild(stand);
-						responses.appendChild(sitstand);
-					}	
-
-					Element blank = document.createElement("blank");
-					blank.appendChild(document.createTextNode(rowTokens[10]));
-					responses.appendChild(blank);
-
-					if (rowTokens.length == 13){
-						Element optional = document.createElement("optional");
-						optional.appendChild(document.createTextNode(rowTokens[12]));
-						responses.appendChild(optional);
-					}
-
-
-
-					question.appendChild(responses);
-
-					questions.appendChild(question);
-				}
-				line++; // increment line count before processing next line    
-			}
-			if (scannerDataFile != null) scannerDataFile.close();
-			Element comments = document.createElement("comments");
-			rootElement.appendChild(comments);
-
-			int commentLine = 0;
-
-			while (scannerCommentsFile.hasNextLine()){
-				String information = scannerCommentsFile.nextLine();
-				String delims = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
-				String[] rowTokens = information.split(delims);
-
-
-				for (String info: rowTokens)
-					System.out.println(info);
-				if (commentLine!=0){
-					Element aComment = document.createElement("aComment");
-					Element commentID = document.createElement("commentNoteID");
-					commentID.appendChild(document.createTextNode(rowTokens[0]));
-					aComment.appendChild(commentID);
-					Element commentDetails = document.createElement("commentDetails");
-					commentDetails.appendChild(document.createTextNode(rowTokens[1]));
-					aComment.appendChild(commentDetails);
-
-					comments.appendChild(aComment);}
-				commentLine++;
-
-			}
-
-			if (scannerCommentsFile != null) scannerCommentsFile.close();
 
 			// This allows saving the DOM as a file with indentation
 			File file = new File(OUTPUT_FILENAME);
@@ -223,6 +115,132 @@ public class csv2xml{
 		catch (Exception exception) {
 			System.err.println("could not create document " + exception);
 		}
+
+	}
+
+
+	private static void appendCommentsToDocument(Element rootElement) {
+		Element comments = document.createElement("comments");
+		rootElement.appendChild(comments);
+
+		int commentLine = 0;
+
+		while (scannerCommentsFile.hasNextLine()){
+			String information = scannerCommentsFile.nextLine();
+			String delims = ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
+			String[] rowTokens = information.split(delims);
+
+
+			for (String info: rowTokens)
+				System.out.println(info);
+			if (commentLine!=0){
+				Element aComment = document.createElement("aComment");
+				Element commentID = document.createElement("commentNoteID");
+				commentID.appendChild(document.createTextNode(rowTokens[0]));
+				aComment.appendChild(commentID);
+				Element commentDetails = document.createElement("commentDetails");
+				commentDetails.appendChild(document.createTextNode(rowTokens[1]));
+				aComment.appendChild(commentDetails);
+
+				comments.appendChild(aComment);}
+			commentLine++;
+
+		}
+
+		if (scannerCommentsFile != null) scannerCommentsFile.close();
+		
+	}
+
+
+	private static void appendSurveyDataToDocument(Element rootElement) {
+		
+
+					//create questions child of library
+					Element questions = document.createElement("questions");
+					rootElement.appendChild(questions);
+
+					// counter for iterating lines of the file
+					int line = 0;
+
+					// while there are more lines in the file read
+					// in the date to be transformed to xml
+					while (scannerDataFile.hasNextLine()){
+
+						String information = scannerDataFile.nextLine();	// extract the line of data
+						String delims = ",";								// csv files are delimeted by commas
+						String[] rowTokens = information.split(delims);
+
+						if (line > 0){	// counting from 1 discards the header row
+
+							appendQuestionToQuestions(questions, rowTokens);
+
+						}
+						line++; // increment line count before processing next line    
+					}
+					if (scannerDataFile != null) scannerDataFile.close();
+		
+	}
+
+
+	private static void appendQuestionToQuestions(Element questions, String[] rowTokens) {
+		// create question element
+		Element question = document.createElement("question");
+
+		// create phrase, comment and append
+		Element phrase = document.createElement("phrase");
+		phrase.appendChild(document.createTextNode(rowTokens[QUESTION]));
+		question.appendChild(phrase);
+
+		if (!(rowTokens[QUESTION_COMMENT].equals(""))){
+			Element commentID = document.createElement("commentID");
+			commentID.appendChild(document.createTextNode(rowTokens[QUESTION_COMMENT]));
+			question.appendChild(commentID);}
+
+		// create answers child of question and append to question
+		Element answers = document.createElement("answers");
+
+
+		if (rowTokens.length == 11){
+			Element yesno = document.createElement("yesno");
+			Element yes = document.createElement("yes");
+			yes.appendChild(document.createTextNode(rowTokens[2]));
+			yesno.appendChild(yes);
+
+			Element no = document.createElement("no");
+			no.appendChild(document.createTextNode(rowTokens[4]));
+			yesno.appendChild(no);
+			responses.appendChild(yesno);
+
+			if (line == 8){
+				Element commentID1 = document.createElement("commentIDno");
+				commentID1.appendChild(document.createTextNode(rowTokens[5]));
+				yesno.appendChild(commentID1);
+			}
+		} else {
+			Element sitstand = document.createElement("sitstand");
+			Element sit = document.createElement("sit");
+			sit.appendChild(document.createTextNode(rowTokens[6]));
+			sitstand.appendChild(sit);
+
+			Element stand = document.createElement("stand");
+			stand.appendChild(document.createTextNode(rowTokens[8]));
+			sitstand.appendChild(stand);
+			responses.appendChild(sitstand);
+		}	
+
+		Element blank = document.createElement("blank");
+		blank.appendChild(document.createTextNode(rowTokens[10]));
+		responses.appendChild(blank);
+
+		if (rowTokens.length == 13){
+			Element optional = document.createElement("optional");
+			optional.appendChild(document.createTextNode(rowTokens[12]));
+			responses.appendChild(optional);
+		}
+
+		question.appendChild(responses);
+
+		questions.appendChild(question);
 
 	}
 }
